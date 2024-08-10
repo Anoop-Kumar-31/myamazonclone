c9dd5caf7450fbe849f489a242870cfc379e02cf
Anoop Kumar <amt312002@gmail.com>
2024-08-11 01:43:21 +0530

    Update page.jsx

--- src/app/Header/page.jsx
+++ src/app/Header/page.jsx
@@ -1,9 +1,10 @@
+'use client';
 import FrontPage from './FrontPage/FrontPage';
 import Header from './Header';
 import {Suspense, useContext} from 'react';
 import checkoutContext from '../context';
 import CheckOut from './Checkout/CheckOut';
-const {isCheckOut} = checkoutContext();
+const {isCheckOut} = useContext(checkoutContext);
 export default function Page() {
     return (
         <main >
