affe52bae40904ae3452e9f1d7a3b4022d22a568
Anoop Kumar <amt312002@gmail.com>
2024-08-11 01:06:16 +0530

    ContextRemoved

--- src/app/Header/BasketDiv.jsx
+++ src/app/Header/BasketDiv.jsx
@@ -3,15 +3,17 @@ import css from './Header.module.css'
 import { useContext } from 'react';
 import { Basket, checkoutContext } from '../context';
 export default function BasketDiv(){
-    const {BasketData, setBasketData} = Basket();
-    const {isCheckOut, setIsCheckOut} = checkoutContext();
-    const len=BasketData.length;
-    const changeElement = () =>{
-        setIsCheckOut(true);
-    }
+    // const {BasketData, setBasketData} = Basket();
+    // const {isCheckOut, setIsCheckOut} = checkoutContext();
+    // const len=BasketData.length;
+    const len=0; //sample value
+    // const changeElement = () =>{
+    //     setIsCheckOut(true);
+    // }
     return(
         <a onClick={changeElement}>
-            <div className={css.inner} onClick={(e)=>{e.preventDefault()}}>
+            {/* <div className={css.inner} onClick={(e)=>{e.preventDefault()}}> */}
+            <div className={css.inner}>
                 <p>{len}</p>
                 <p>Cart</p>
             </div>
