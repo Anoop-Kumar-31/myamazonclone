b3e5f227dc5682e6fe6d9f5f3ea3eb18423d7179
Anoop Kumar <amt312002@gmail.com>
2024-08-11 17:05:02 +0530

    More

--- src/app/Header/FrontPage/Cards/Card.jsx
+++ src/app/Header/FrontPage/Cards/Card.jsx
@@ -1,20 +1,20 @@
 
 import css from './Cards.module.css'
 import { useContext } from 'react';
-import { Basket } from '../../../context';
+import { Basket } from '../../../page';
 export default function Card({image,name,cost}) {
-    // const {BasketData, setBasketData} = Basket();
-    // const addtocart = () =>{
-    //     setBasketData([...BasketData,{image,name,cost}]);
-    //     window.localStorage.setItem('BasketData',JSON.stringify([...BasketData,{image,name,cost}]));
-    // }
+    const {BasketData, setBasketData} = Basket();
+    const addtocart = () =>{
+        setBasketData([...BasketData,{image,name,cost}]);
+        window.localStorage.setItem('BasketData',JSON.stringify([...BasketData,{image,name,cost}]));
+    }
     return(
         <div className={css.card}>
             <img src={image} alt='productimage'></img>
             <h3>{name}</h3>
             <h4>{'₹'+cost}</h4>
-            {/* <button onClick={addtocart}>Add to Cart</button> */}
-            <button>Add to Cart</button>
+            <button onClick={addtocart}>Add to Cart</button>
+            {/* <button>Add to Cart</button> */}
         </div>
     )
 }
