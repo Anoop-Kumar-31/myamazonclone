b077ab2eca40b4937ac8181a13a384b1810befda
Anoop Kumar <amt312002@gmail.com>
2024-08-11 16:36:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: false,
+    // reactStrictMode: true,
+    // images: {
+    //     domains: ['m.media-amazon.com'],
+    // },
+    async headers() {
+        return [
+        {
+            source: '/api/:path*',
+            headers: [
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept' },
+            ],
         },
+        ];
+    },
 };
 export default nextConfig;
