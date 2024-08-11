203eb4386e44bc22321f6fc3e48ab51ee9e70e27
Anoop Kumar <amt312002@gmail.com>
2024-08-11 17:35:28 +0530

    Summary (required)

--- src/app/Header/page.jsx
+++ src/app/Header/page.jsx
@@ -2,7 +2,7 @@
 import FrontPage from './FrontPage/FrontPage';
 import Header from './Header';
 import {Suspense, useContext} from 'react';
-import {checkoutContext, mycheckoutContext} from '../page';
+import {checkoutContext} from '../page';
 import CheckOut from './Checkout/CheckOut';
 export default function Page() {
     const {isCheckOut} = checkoutContext();
