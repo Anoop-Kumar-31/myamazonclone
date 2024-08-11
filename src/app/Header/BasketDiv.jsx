db8131620a8cfa25b42d6f21344102378793cf00
Anoop Kumar <amt312002@gmail.com>
2024-08-11 17:09:23 +0530

    new

--- src/app/Header/BasketDiv.jsx
+++ src/app/Header/BasketDiv.jsx
@@ -1,22 +1,20 @@
 import css from './Header.module.css'
 // import { Basket} from '../page';
-import { Basket, checkoutContext } from '../page';
+import { useContext } from 'react';
+import { Basket, checkoutContext } from '../context';
 export default function BasketDiv(){
-    const {BasketData, setBasketData} = Basket();
-    const {isCheckOut, setIsCheckOut} = checkoutContext();
-    try{const len=BasketData.length;}
-    catch(e){
-        const len=0;
-    }
-    // const len=0; //sample value
-    const changeElement = () =>{
-        setIsCheckOut(true);
-    }
+    // const {BasketData, setBasketData} = Basket();
+    // const {isCheckOut, setIsCheckOut} = checkoutContext();
+    // const len=BasketData.length;
+    const len=0; //sample value
+    // const changeElement = () =>{
+    //     setIsCheckOut(true);
+    // }
     return(
-        <a onClick={changeElement}>
-        {/* <a> */}
-            <div className={css.inner} onClick={(e)=>{e.preventDefault()}}>
-            {/* <div className={css.inner}> */}
+        // <a onClick={changeElement}>
+        <a>
+            {/* <div className={css.inner} onClick={(e)=>{e.preventDefault()}}> */}
+            <div className={css.inner}>
                 <p>{len}</p>
                 <p>Cart</p>
             </div>
