5404b5ba615515744dc4b058ee54dcd79cd530f8
Anoop Kumar <amt312002@gmail.com>
2024-08-11 16:45:09 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import styles from "./page.module.css";
 import Header from "./Header/page";
 // import FrontPage from "./Header/FrontPage/page";
 // import { useState, createContext, useEffect } from "react";
-// import {} from "react";
+
+import {useContext} from "react";
 import { mycheckoutContext,myBasket } from "./context";
 import MyContextProvider from "./context";
 // const mycheckoutContext = createContext();
