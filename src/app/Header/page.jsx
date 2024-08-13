e389bbef69629971b426b8faa99d79f17d2b28d4
Anoop Kumar <amt312002@gmail.com>
2024-08-14 00:45:08 +0530

    Update page.jsx

--- src/app/Header/page.jsx
+++ src/app/Header/page.jsx
@@ -3,7 +3,7 @@ import FrontPage from './FrontPage/FrontPage';
 import Header from './Header';
 import {Suspense, useContext} from 'react';
 import {checkoutContext} from '../page';
-import { Basket } from './Header/BasketDiv';
+import { Basket } from '../page';
 import CheckOut from './Checkout/CheckOut';
 export default function Page() {
     const {isCheckOut} = checkoutContext();
