16aa1953b8a3c9d83d916166c529c5537a6eb1ae
Anoop Kumar <amt312002@gmail.com>
2024-08-14 01:03:39 +0530

    Update page.jsx

--- src/app/Header/page.jsx
+++ src/app/Header/page.jsx
@@ -6,10 +6,10 @@ import {checkoutContext} from '../page';
 import { Basket } from '../page';
 import CheckOut from './Checkout/CheckOut';
 export default async function Page() {
-    const {isCheckOut} = checkoutContext();
-    const {basketData, setBasketData} = await Basket==undefined?{basketData:["amit"], setBasketData:()=>{}}:Basket();
-    const len=basketData?basketData.length:0;
-    console.log(len);
+    // const {isCheckOut} = checkoutContext();
+    // const {basketData, setBasketData} = await Basket==undefined?{basketData:["amit"], setBasketData:()=>{}}:Basket();
+    // const len=basketData?basketData.length:0;
+    // console.log(len);
     return (
         <main >
         <Suspense fallback={<div>Loading...</div>}>
