7c34f70cd8d19aae7c1e98cc285d337154d125d7
Anoop Kumar <amt312002@gmail.com>
2024-08-14 00:49:38 +0530

    Update page.jsx

--- src/app/Header/page.jsx
+++ src/app/Header/page.jsx
@@ -7,7 +7,7 @@ import { Basket } from '../page';
 import CheckOut from './Checkout/CheckOut';
 export default function Page() {
     const {isCheckOut} = checkoutContext();
-    const {basketData, setBasketData} = Basket();
+    const {basketData, setBasketData} = Basket?Basket():{basketData:["amit"], setBasketData:()=>{}};
     const len=basketData.length;
     console.log(len);
     return (
