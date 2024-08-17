a6a292fc8bdeaa5b4f06a77c004946f1e77ec0e2
Anoop Kumar <amt312002@gmail.com>
2024-08-17 18:43:33 +0530

    Update BasketDiv.jsx

--- src/app/Header/BasketDiv.jsx
+++ src/app/Header/BasketDiv.jsx
@@ -2,11 +2,11 @@ import css from './Header.module.css'
 import React from 'react';
 import { Basket, checkoutContext } from '../page';
 export default function BasketDiv(){
-    const {basketData, setBasketData} = Basket();
-    const {isCheckOut, setIsCheckOut} = checkoutContext();
+    // const {basketData, setBasketData} = Basket();
+    // const {isCheckOut, setIsCheckOut} = checkoutContext();
     // const len=0; //sample value
-    // const {basketData}= Basket!=undefined? Basket():{basketData:[]};
-    // const {setIsCheckOut }= checkoutContext!=undefined? checkoutContext():{setIsCheckOut:()=>{}};
+    const {basketData}= Basket!=undefined? Basket():{basketData:[]};
+    const {setIsCheckOut }= checkoutContext!=undefined? checkoutContext():{setIsCheckOut:()=>{}};
     const len=basketData.length;
     const changeElement = () =>{
         setIsCheckOut(true);
