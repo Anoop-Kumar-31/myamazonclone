edf053d0c4449d42ed1e272d8bc86ca1f41f2e86
Anoop Kumar <amt312002@gmail.com>
2024-08-17 18:39:01 +0530

    Update BasketDiv.jsx

--- src/app/Header/BasketDiv.jsx
+++ src/app/Header/BasketDiv.jsx
@@ -2,9 +2,9 @@ import css from './Header.module.css'
 import React from 'react';
 import { Basket, checkoutContext } from '../page';
 export default function BasketDiv(){
-    // const {basketData, setBasketData} = Basket();
-    // const {isCheckOut, setIsCheckOut} = checkoutContext();
-    // const len=0; //sample value
+    const {basketData, setBasketData} = Basket();
+    const {isCheckOut, setIsCheckOut} = checkoutContext();
+    const len=0; //sample value
     // const {basketData}= Basket!=undefined? Basket():{basketData:[]};
     // const {setIsCheckOut }= checkoutContext!=undefined? checkoutContext():{setIsCheckOut:()=>{}};
     // const len=basketData.length;
@@ -12,11 +12,11 @@ export default function BasketDiv(){
         // setIsCheckOut(true);
     }
     return(
-        <a>
-            <div className={css.inner}>
+        // <a>
+        //     <div className={css.inner}>
             
-        {/* <a onClick={changeElement}>
-            <div className={css.inner} onClick={(e)=>{e.preventDefault()}}> */}
+        <a onClick={changeElement}>
+            <div className={css.inner} onClick={(e)=>{e.preventDefault()}}>
                 <p>{len}</p>
                 <p>Cart</p>
             </div>
